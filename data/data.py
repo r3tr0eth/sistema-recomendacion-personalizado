32bc00fc82aa2c0c835c2f20dbed1be10c5178f9
Gaizkaja <jimenezgaizka13@protonmail.com>
2024-01-08 19:13:59 +0000

    FEAT first analysis

---
+++ data/data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Cargar tus datos
+df = pd.read_csv('data/movies.csv')
+
+# Distribución de Géneros
+print(df['Genre'].value_counts())
+
+# Relación entre Rentabilidad y Puntuación de la Audiencia
+sns.scatterplot(x='Audience score %', y='Profitability', data=df)
+
+# Histograma de Puntuaciones de Rotten Tomatoes
+df['Rotten Tomatoes %'].hist(bins=20)
+
+# Tendencias a lo largo del tiempo
+sns.lineplot(x='Year', y='Profitability', data=df)
+
+plt.show()
